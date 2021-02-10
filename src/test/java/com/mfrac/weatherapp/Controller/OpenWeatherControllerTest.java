3441e2d02620de6bb36cb00bee9695d17f1b75a3
Marcin Frac <marcin.frac.ctr@sabre.com>
2021-02-10 18:14:39 +0100

    unit test update

--- src/test/java/com/mfrac/weatherapp/Controller/OpenWeatherControllerTest.java
+++ src/test/java/com/mfrac/weatherapp/Controller/OpenWeatherControllerTest.java
@@ -1,14 +1,23 @@
 package com.mfrac.weatherapp.Controller;
 
 import com.mfrac.weatherapp.Config.OpenWeatherConfiguration;
+import com.mfrac.weatherapp.Mapper.WeatherAppMapper;
+import com.mfrac.weatherapp.Model.ForecastResponse;
+import com.mfrac.weatherapp.Model.WeatherAppResponse;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OpenWeatherControllerTest {
@@ -23,16 +32,16 @@ public class OpenWeatherControllerTest {
     @InjectMocks
     OpenWeatherController openWeatherController;
 
-/*    @Test
+    @Test
     public void shouldReturnHttpStatusOk() {
         //given
         when(openWeatherConfiguration.getToken()).thenReturn("token");
         when(openWeatherConfiguration.getUrl()).thenReturn("url");
-        when(restTemplate.exchange("url/weather?id={id}&appid={appid}&lang={lang}&mode={mode}", HttpMethod.GET, httpEntity, String.class, "756135", "token", "pl", "json"))
+        when(restTemplate.exchange("url/weather?id={id}&appid={appid}&lang={lang}&mode={mode}", HttpMethod.GET, httpEntity, ForecastResponse.class, "756135", "token", "en", "json"))
                 .thenReturn(new ResponseEntity<>(HttpStatus.OK));
         //when
-        ResponseEntity<String> byCityId = openWeatherController.getByCityId("756135", "pl", "json");
+        ResponseEntity<WeatherAppResponse> byCityId = openWeatherController.getByCityId("756135", "en", "json");
         //then
         assertThat(byCityId.getStatusCode().value()).isEqualTo(200);
-    }*/
+    }
 }
