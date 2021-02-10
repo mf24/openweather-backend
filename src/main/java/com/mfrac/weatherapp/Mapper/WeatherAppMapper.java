19d9fbb9d0238e9e1eb76dd73267d786ae44bf5b
Marcin Frac <marcin.frac.ctr@sabre.com>
2021-02-10 19:43:06 +0100

    code refactor

--- src/main/java/com/mfrac/weatherapp/Mapper/WeatherAppMapper.java
+++ src/main/java/com/mfrac/weatherapp/Mapper/WeatherAppMapper.java
@@ -3,16 +3,17 @@ package com.mfrac.weatherapp.Mapper;
 import com.mfrac.weatherapp.Model.Error;
 import com.mfrac.weatherapp.Model.ForecastResponse;
 import com.mfrac.weatherapp.Model.WeatherAppResponse;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import static java.util.Objects.nonNull;
+import java.util.Optional;
 
 @Service
 public class WeatherAppMapper {
     public WeatherAppResponse mapBasicForecastData(ResponseEntity<ForecastResponse> response) {
 
-        if (nonNull(response.getBody())) {
+        if (Optional.ofNullable(response).map(HttpEntity::getBody).isPresent()) {
             return new WeatherAppResponse.Builder()
                     .withCity(response.getBody().getName())
                     .withDescription(response.getBody().getWeather().get(0).getDescription())
