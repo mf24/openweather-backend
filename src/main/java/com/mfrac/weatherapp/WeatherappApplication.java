59eb415f797e50a954aecdce8d8f915126f8fab0
Marcin Frac <marcin.frac.ctr@sabre.com>
2021-02-11 14:45:48 +0100

    origin allowance set to *

--- src/main/java/com/mfrac/weatherapp/WeatherappApplication.java
+++ src/main/java/com/mfrac/weatherapp/WeatherappApplication.java
@@ -3,6 +3,7 @@ package com.mfrac.weatherapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class WeatherappApplication {
 
 	public static void main(String[] args) {
@@ -21,7 +23,7 @@ public class WeatherappApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
